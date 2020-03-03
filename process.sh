ebd7b7204231c4e3958ca6669a1f790d9fd3c3d5
Abdellah Fourtassi <afourtassi@Abdellahs-MacBook-Pro.local>
2020-03-03 16:53:58 +0100

    variable

--- process.sh
+++ process.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find nadig/ -name "*.cha" | while read -r file; do cat "$file" |  grep '^\*' | sed 's/^\*//g' > $file.conv ; done
+corpus=$1
+
+find $corpus/ -name "*.cha" | while read -r file; do cat "$file" |  grep '^\*' | sed 's/^\*//g' > $file.conv ; done
