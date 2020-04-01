c07949c5f1bce00c59e49904a4e969c8015c661f
Pelayo Mencos <mencospelayo@gmail.com>
2020-04-01 13:56:07 +0200

    change participants to participant

--- process.sh
+++ process.sh
@@ -4,6 +4,6 @@ corpus=$1
 
 find $corpus/ -name "*.cha" | while read -r file; do cat "$file" |  grep '^\*' | sed 's/^\*//g' > $file.conv ; done
 
-find $corpus/ -name "*.conv" | while read -r file; do { echo -e "participants\tcontent"; cat "$file"; } > $file.txt ; done
+find $corpus/ -name "*.conv" | while read -r file; do { echo -e "participant\tcontent"; cat "$file"; } > $file.txt ; done
 
 find $corpus/ -name "*.conv" -type f -delete
