5cebf793d4ca2cafad2629ee289bcc274bbc789f
Abdellah Fourtassi <afourtassi@Abdellahs-MacBook-Pro.local>
2020-04-10 14:22:48 +0200

    new processing

--- process.sh
+++ process.sh
@@ -2,7 +2,7 @@
 
 corpus=$1
 
-find $corpus/ -name "*.cha" | while read -r file; do cat "$file" |  grep '^\*' | sed 's/^\*//g' > $file.conv ; done
+find $corpus/ -name "*.cha" | while read -r file; do cat "$file" |  grep '^\*' | sed 's/^\*//g' | sed 's/xxx//g' | sed 's/yyy//g' | sed 's/www//g' | sed -E 's/\(.+\)//g' | sed 's/[\<\>]//g'  | sed -E 's/&=?-?\+?[a-z]+//g' | sed -E 's/\[.+\]//g' | sed 's/://g' | sed -E 's/@[a-z]+//g' | sed -E 's/[.,\!\?\+\/0]//g' > $file.conv ; done
 
 find $corpus/ -name "*.conv" | while read -r file; do { echo -e "participant\tcontent"; cat "$file"; } > $file.txt ; done
 
